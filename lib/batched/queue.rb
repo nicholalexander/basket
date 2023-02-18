504f5a6c88b8af0ba96f0312a3b18eea3772a05d
nichol alexander <nichol.alexander@gmail.com>
2023-02-17 22:08:41 -0500

    a queue for now.

---
+++ lib/batched/queue.rb
@@ -0,0 +1,26 @@
+module Batched
+  class Queue
+    def initialize(backend = HashBackend.new)
+      @backend = backend
+    end
+
+    def push(queue, data)
+      @backend.push(queue, data)
+      length(queue)
+    end
+
+    def length(queue)
+      @backend.length(queue)
+    end
+
+    def pop_all(queue)
+      @backend.pop_all(queue)
+    end
+
+    def data
+      @backend.data
+    end
+  end
+end
+
+
