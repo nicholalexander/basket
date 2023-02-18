046a14b52528b92bc2c44a6e315684b3bf2b5b1f
nichol alexander <nichol@mudflapinc.com>
2023-02-18 13:35:53 -0500

    some pending tests.

---
+++ spec/basket/queue_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe Basket::Queue do
+  describe "#push" do
+    it "adds an element to the specified queue"
+    it "returns the length of the queue"
+  end
+
+  describe "#length" do
+    it "returns the length of the specified queue"
+  end
+
+  describe "#pop_all" do
+    it "returns all the elements in the specified queue"
+  end
+end
