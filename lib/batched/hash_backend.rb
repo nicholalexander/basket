e0930149e3bb2cd96e9127059b423c1661c6adde
nichol alexander <nichol.alexander@gmail.com>
2023-02-17 22:08:55 -0500

    backends - this is a simple hash.
    this is how i'm starting to think we'd add in redis or something.

---
+++ lib/batched/hash_backend.rb
@@ -0,0 +1,23 @@
+module Batched
+  class HashBackend
+    attr_reader :data
+
+    def initialize
+      @data = {}
+    end
+
+    def push(queue, data)
+      @data[queue] = [] if @data[queue].nil?
+      @data[queue] <<= data
+    end
+
+    def length(queue)
+      return 0 if @data[queue].nil?
+      @data[queue].length
+    end
+
+    def pop_all(queue)
+      @data.delete(queue)
+    end
+  end
+end
