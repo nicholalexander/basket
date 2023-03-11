d1390283ca554a3097a640e2ecc5f77bc327d702
nichol alexander <nichol.alexander@gmail.com>
2023-03-11 08:51:15 -0500

    defaults and some tests.

--- lib/basket/configuration.rb
+++ lib/basket/configuration.rb
@@ -1,11 +1,23 @@
 module Basket
   class Configuration
-    def queue_collection
-      @queue_collection ||= Basket::QueueCollection.new
+    attr_accessor :redis_host, :redis_port, :redis_db
+    attr_reader :backend
+
+    def initialize
+      # Defaults
+      @redis_host = "127.0.0.1"
+      @redis_port = 6379
+      @redis_db = 15
+      @backend = BackendAdapter::HashBackend
     end
 
-    def backend
-      @backend ||= BackendAdapter::RedisBackend
+    def backend=(backend)
+      case backend
+      when :hash
+        @backend = BackendAdapter::HashBackend
+      when :redis
+        @backend = BackendAdapter::RedisBackend
+      end
     end
   end
 end
