8d1e4154cdb9230a0db55d17fa85d7f6466080bd
nichol alexander <nichol.alexander@gmail.com>
2023-03-26 11:12:59 -0400

    update basket ver in gemfile and refactor handle_add

--- lib/basket/handle_add.rb
+++ lib/basket/handle_add.rb
@@ -7,30 +7,37 @@ module Basket
     def initialize(queue, data)
       @queue = queue
       @data = data
-      @queue_collection = Basket.queue_collection
     end
 
     def call(data = @data)
-      queue_length = @queue_collection.push(@queue, data)
-      queue_class = class_for_queue
-      queue_instance = queue_class.new
-
-      queue_instance.define_singleton_method(:element) { data }
-      queue_instance.on_add
-
-      return unless basket_full?(queue_length, queue_class)
-
-      queue_instance.perform
-      queue_instance.on_success
-      @queue_collection.clear(@queue)
-    rescue => e
-      raise e if basket_error?(e)
-
-      queue_instance.define_singleton_method(:error) { e }
-      queue_instance.on_failure
+      setup_batchers
+      run_lifecycle
     end
 
     private
+
+    def setup_batchers
+      @queue_collection = Basket.queue_collection
+      @queue_class = class_for_queue
+      @queue_instance = @queue_class.new
+    end
+
+    def run_lifecycle(data = @data)
+      queue_length = @queue_collection.push(@queue, data)
+      @queue_instance.define_singleton_method(:element) { data }
+      @queue_instance.on_add
+
+      return unless basket_full?(queue_length, @queue_class)
+
+      @queue_instance.perform
+      @queue_instance.on_success
+      @queue_collection.clear(@queue)
+    rescue => error
+      raise error if basket_error?(error)
+
+      @queue_instance.define_singleton_method(:error) { error }
+      @queue_instance.on_failure
+    end
 
     def class_for_queue
       Object.const_get(@queue)
