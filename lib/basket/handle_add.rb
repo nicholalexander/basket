1b3faba92895d5aa9cc472ac7a8bc80f9b5aa452
nichol alexander <nichol.alexander@gmail.com>
2023-03-25 22:20:08 -0400

    change signature and use private methods

--- lib/basket/handle_add.rb
+++ lib/basket/handle_add.rb
@@ -1,28 +1,43 @@
 module Basket
   class HandleAdd
+    def self.call(queue, data)
+      new(queue, data).call
+    end
+
     def initialize(queue, data)
       @queue = queue
       @data = data
+      @queue_collection = Basket.queue_collection
     end
 
     def call(queue = @queue, data = @data)
-      queue_length = Basket.queue_collection.push(queue, data)
-      queue_class = Object.const_get(queue)
+      queue_length = @queue_collection.push(queue, data)
+      queue_class = class_for_queue
       queue_instance = queue_class.new
 
       queue_instance.define_singleton_method(:element) { data }
       queue_instance.on_add
 
-      return unless queue_length == queue_class.basket_options_hash[:size]
+      return unless basket_full?(queue_length, queue_class)
 
       queue_instance.perform
       queue_instance.on_success
-      Basket.queue_collection.clear(queue)
+      @queue_collection.clear(queue)
     rescue => e
       raise e if e.instance_of?(Basket::Error)
 
       queue_instance.define_singleton_method(:error) { e }
       queue_instance.on_failure
+    end
+
+    private
+
+    def class_for_queue
+      Object.const_get(@queue)
+    end
+
+    def basket_full?(queue_length, queue_class)
+      queue_length == queue_class.basket_options_hash[:size]
     end
   end
 end
