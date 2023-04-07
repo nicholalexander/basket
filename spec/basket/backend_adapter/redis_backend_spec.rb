c1e33d855688c4295ea586c18978587e89f067e5
Alec Clarke <alec.clarke.dev@gmail.com>
2023-04-07 16:31:07 -0400

    Serialize/Deserialize redis data via JSON
    Serialize/Deserialize redis data via JSON as it's a more stable/predictable serialization compared to Marshalling.

--- spec/basket/backend_adapter/redis_backend_spec.rb
+++ spec/basket/backend_adapter/redis_backend_spec.rb
@@ -2,20 +2,20 @@ RSpec.describe Basket::BackendAdapter::RedisBackend do
   describe "#data" do
     it "returns all the basket entries" do
       backend = described_class.new
-      backend.push("test_queue_1", {a: 1})
-      backend.push("test_queue_1", {a: 2})
-      backend.push("test_queue_2", {b: 1})
+      backend.push("test_queue_1", {"a" => 1})
+      backend.push("test_queue_1", {"a" => 2})
+      backend.push("test_queue_2", {"b" => 1})
 
       expect(backend.data).to eq({
-        "test_queue_1" => [{a: 1}, {a: 2}],
-        "test_queue_2" => [{b: 1}]
+        "test_queue_1" => [{"a" => 1}, {"a" => 2}],
+        "test_queue_2" => [{"b" => 1}]
       })
     end
   end
 
   describe "#push" do
     it "pushes an item into the given queue" do
-      result = described_class.new.push("test_queue", {a: 1})
+      result = described_class.new.push("test_queue", {"a" => 1})
 
       expect(result).to eq(1)
     end
@@ -24,8 +24,8 @@ RSpec.describe Basket::BackendAdapter::RedisBackend do
   describe "#length" do
     it "returns the length of the given queue" do
       backend = described_class.new
-      backend.push("test_queue", {a: 1})
-      backend.push("test_queue", {b: 2})
+      backend.push("test_queue", {"a" => 1})
+      backend.push("test_queue", {"b" => 2})
 
       expect(backend.length("test_queue")).to eq(2)
     end
