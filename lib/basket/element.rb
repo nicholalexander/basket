7207e5fa079ec5974109a58ee4ac86ecb69eaada
Alec Clarke <alec.clarke.dev@gmail.com>
2023-04-16 17:16:25 -0400

    Build out and test Element for better error handling

--- lib/basket/element.rb
+++ lib/basket/element.rb
@@ -2,6 +2,8 @@ require "securerandom"
 
 module Basket
   class Element
+    class InvalidElement < StandardError; end
+
     attr_reader :data, :id
 
     def self.from_queue(element)
@@ -9,18 +11,29 @@ module Basket
         element
       else
         parsed_element = JSON.parse(element)
-
         new(parsed_element["data"], parsed_element["id"])
       end
+    rescue JSON::ParserError => e
+      raise InvalidElement, "failed to parse element to json: #{e.message} "
     end
 
     def initialize(data, id = SecureRandom.uuid)
+      raise InvalidElement, "both data and id must be present" unless data && id
+
       @data = data
       @id = id
     end
 
+    def to_h
+      {data: data, id: id}
+    end
+
     def to_json(*)
-      {data: data, id: id}.to_json
+      to_h.to_json
+    end
+
+    def ==(other)
+      to_h == other.to_h
     end
   end
 end
